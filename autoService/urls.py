24dc49d8c1c890d978e32d4a64cc965b0df76ddf
HiKKors <nk041496@gmail.com>
2026-02-08 13:44:59 +0400

    Update autoService app urls

--- autoService/urls.py
+++ autoService/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .client_views import AllServicesList, ServiceDetail, create_booking, add_booking_detail, mark_arrived, BookingManagment, PendingBookings
-from .owner_views import MyServicesView, MyServiceManagmentView, EditAutoServiceView, get_service_mounth_report_view, AllBookingsView, add_auto_service
+from .client_views import AllServicesList, ServiceDetail, create_booking, add_booking_detail, mark_arrived, BookingManagment, PendingBookings, mark_completed, get_boxes, get_available_times
+from .owner_views import add_staff_view, MyServicesView, MyServiceManagmentView, EditAutoServiceView, get_service_mounth_report_view, AllBookingsView, add_auto_service, MyStaffView, MyServicesForStaffView, ServiceAnalytics
 
 urlpatterns = [
     # CLIENT URLS
@@ -11,7 +11,12 @@ urlpatterns = [
     path('create-booking/<int:service_id>/add_booking_detail/<int:booking_id>', add_booking_detail, name='add_booking_detail'),
     path('booking_managment/<int:pk>', BookingManagment.as_view(), name='booking_managment'),
     path('mark_arrived/<int:booking_id>', mark_arrived, name='mark_arrived'),
+    path('mark_completed/<int:booking_id>', mark_completed, name='mark_completed'),
     path('pending_bookings/', PendingBookings.as_view(), name='pending_bookings'),
+    
+    #test booking
+    path('api/boxes/', get_boxes, name='api_boxes'),
+    path('api/available-times/', get_available_times, name='api_available_times'),
     
     # ADMIN URLS
     path('service-admin/create-autoservice/', add_auto_service, name='create_autoservice'),
@@ -20,4 +25,8 @@ urlpatterns = [
     path('service-admin/my-services/managment/<int:pk>/edit', EditAutoServiceView.as_view(), name='edit_autoservice'),
     path('service-admin/my-services/create-report/<int:service_id>', get_service_mounth_report_view, name='service-report'),
     path('service-admin/all-bookings/', AllBookingsView.as_view(), name='all_bookings'),
+    path('service-admin/services_staff/', MyServicesForStaffView.as_view(), name='services_staff'),
+    path('service-admin/services_staff/<int:service_id>', MyStaffView.as_view(), name='my_staff'),
+    path('service-admin/services_staff/<int:service>/add', add_staff_view, name='add_staff'),
+    path('service-admin/service-analytics/<int:service_id>', ServiceAnalytics.as_view(), name='service_analytics')
 ]
