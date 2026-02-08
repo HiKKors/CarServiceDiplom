87b371c4284c8e4a5c9032a54c383f97e55663b8
HiKKors <nk041496@gmail.com>
2026-02-08 13:40:11 +0400

    Add expense form

--- analytics/forms.py
+++ analytics/forms.py
@@ -4,11 +4,11 @@ from .models import Expenses
 class AddExpenseForm(forms.ModelForm):
     class Meta:
         model = Expenses
-        fields = ['category', 'amount', 'date', 'description']
+        fields = ['category', 'amount', 'description']
         
         widgets = {
             'category': forms.Select(attrs={'class': 'form-select'}),
             'amount': forms.NumberInput(attrs={'class': 'form-control'}),
-            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            # 'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
